ea1c36b3284b9fb598e95317a5154d472643a5d2
Christina <bihtya@mail.ru>
2023-02-08 22:38:59 +0300

    Записали решение первой задачи

--- Exp01/Program.cs
+++ Exp01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int ValueInput()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void GetNatNumber(int value)
+{
+    if (value >= 1)
+    {
+        Console.Write($"{value} ");
+        GetNatNumber(value - 1);
+    }
+
+}
+int number = ValueInput();
+GetNatNumber(number);
